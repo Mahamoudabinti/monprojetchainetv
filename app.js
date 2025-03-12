c97208fb1d6d13590fdd7bdbbba69b8dc8e9309a
Mahamoudabinti <jasmine.mahamouda@gmail.com>
2025-03-12 09:31:30 +0400

    progres de travail

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
 //la variable expresse vas stocker la librairie express
 const express = require("express");
 
+// la variable mysql vas stocker la librairie mysql
+const mysql = require('mysql2');
+//la variable express-myconnection permet de se connecter dans mysql.
+const myConnection = require('express-myconnection');
+
+
+const database = require("mime-db");
+const connection = require("express-myconnection");
+
 const app = express();
 // Configuration du moteur de vue
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+
+//------PREPARATION A LA CONNECTION A LA BASE DE DONNEEs.
+//je configure les paramètres de connection à MysQL Server
+const optionConnection = {
+    host:"localhost", //le host, c'est l'endroit ou ma base de donnée
+    user:"root",
+    password:"Jasmine.28",
+    database:"chainetv",
+    port:3306
+};
+
+//on utilise la librairie myconnection pour pouvoir recuperer dans mysql,optionconnection et pool.
+app.use(myConnection(mysql, optionConnection, "pool"));
+
+//-------FIN DE LA CONNECTION A LA BASE DE DONNEES.
+
 // Middleware pour servir des fichiers statiques
 app.use(express.static('public'));
 
-// creée une route 
+//  Cette ligne définit une route HTTP GET pour le chemin "/accueil". 
+// Lorsque cette route est accédée, la fonction qui suit est exécutée.
 app.get("/accueil", (req, res) => {
     res.render("accueil"); // Rendre la page à accueil
 });
 app.get('/apropos', (req, res) => {
-    res.render('apropos'); // Rendre la page à apropos
+    req.getConnection((erreur, connection)=>{ //Ici, on demande une connexion à la base de données. 
+    // La fonction prend deux arguments : une erreur potentielle et la connexion à la base de données.
+
+        if (erreur){
+            console.log(erreur); 
+        }else {
+            connection.query("SELECT * FROM equipe", [], (err, equipe) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    res.render('apropos', {equipe}); // Rendre la page à apropos
+                    
+
+                }
+            })
+        }
+    })
+   
 });
 app.get('/navbar', (req, res) => {
+
     res.render('navbar'); // Rendre la page à navbar
 });
 app.get('/programmeTv', (req, res) => {
--- sql/requete.sql
+++ sql/requete.sql
@@ -16,3 +16,44 @@ Create Table programmediffusion (
     description varchar(255),
     date_diffusion Datetime not null
 );
+
+-- insérer des membres dans la table equipe
+INSERT INTO equipe(id, nom, prenom, poste)
+VALUES
+(1, "jasmine", "binti","directrice générale"),
+(2, "soulaimana", "Soumaya", "chef d'émission"),
+(3, "benli", "Samir","programmeur d'émission"),
+(4, "soulaimana", "samnoune", "ingénieur son"),
+(5, "saltnoune", "Tatiana", "réalisateur"),
+(6, "mahamouda", "Houssam", "camera men"),
+(7, "mahamouda", "Inchati", "camera men");
+
+
+--  inserer des programmes dans la table programmediffusion
+
+INSERT INTO programmediffusion(id, titre, description, date_diffusion)VALUES
+(1, "deba", " déba est une forme musicale pratiquée par les communautés malgaches. 
+Il est lié à des cérémonies culturelles et des célébrations. Accompagné d'instruments comme des tambours. 
+ C'est un moyen de transmettre des valeurs culturelles", "2025-03-28 20:00:00"),
+
+ (2, "moulidi", "Le Moulidi à Mayotte célèbre la naissance du prophète Mouhammad. 
+ C'est une fête importante pour la communauté musulmane, 
+ marquée par des prières, des chants religieux et des récitations du Coran.", "2025-02-13 20:35:00"),
+
+  (3, "dahira", "La dahira à Mayotte est une association communautaire qui se concentre 
+  sur des valeurs religieuses,culturelles ou sociales.
+    Elle organise des activités comme des cours de Coran, 
+    des conférences et des événements culturels.", "2025-01-03 21:30:00"),
+
+  (4, "maoulida sheingue", "Le Maoulida Sheingue à Mayotte célèbre la naissance du prophète Mouhammad. 
+  C'est une fête marquée par des chants, des prières et des récitations du Coran.
+   Les gens se rassemblent pour partager des repas et célébrer ensemble.", "2025-07-09 21:50:00"),
+
+   (5, "m'biwi", 
+   "Elles expriment des émotions comme l'amour et la joie, 
+    Les paroles, en shimaore,
+    racontent des histoires de la vie quotidienne et des traditions.
+     Accompagnées de rythmes entraînants et de danses.", "2025-04-01 22:39:00");
+
+-- afficher les tables du programmediffsion.
+     SELECT * FROM programmediffusion;
